994edb51ba88dc2cb0cc8ce059ed0030d48f05e1
Imran Toprak <narmi1998@gmail.com>
2022-10-19 11:06:00 +0100

    Pone_upd

--- imran.R
+++ imran.R
@@ -1,4 +1,3 @@
-#FOO
 Pone <- function(n, k, strategy, nreps) { 
   #Initialization
   counter <- rep(0, nreps)   
@@ -80,9 +79,80 @@ Pone <- function(n, k, strategy, nreps) {
   print(sum(counter)/nreps)
 }
 
-Pone(5,3,1,10000)
-Pone(5,3,2,10000)
-Pone(5,3,3,10000)
+#Pone(5,3,1,10000)
+#Pone(5,3,2,10000)
+#Pone(5,3,3,10000)
+
+#FOO
+Pone <- function(n, k, strategy, nreps) { 
+  #Initialization
+  counter <- rep(0, nreps)   
+  
+  if (strategy == 1) {
+    for (a in 1:nreps) {
+      #Card Box Allocation
+      box_numbers <- c(1:(2*n))
+      card_numbers <- c(sample(1:(2*n)))
+      cards_in_boxes <- list()
+      open_order <- c(k)
+      for (i in 1:(2*n)) {
+        cards_in_boxes <- append(cards_in_boxes, list(c(box_numbers[i], card_numbers[i])))
+      }
+      for (j in 1:n) {
+        if (cards_in_boxes[[open_order[j]]][2] == k) {
+          counter[a] <- 1
+          break}
+        else {
+          open_order <- append(open_order, cards_in_boxes[[open_order[j]]][2])
+        }      
+      }
+    }
+  }
+  
+  if (strategy == 2) {
+    for (a in 1:nreps) {
+      #Card Box Allocation
+      box_numbers <- c(1:(2*n))
+      card_numbers <- c(sample(1:(2*n)))
+      cards_in_boxes <- list()      
+      open_order <- c(sample(1:(2*n), 1))
+      for (i in 1:(2*n)) {
+        cards_in_boxes <- append(cards_in_boxes, list(c(box_numbers[i], card_numbers[i])))
+      }
+      for (j in 1:n) {
+        if (cards_in_boxes[[open_order[j]]][2] == k) {
+          counter[a] <- 1
+          break}
+        else {
+          open_order <- append(open_order, cards_in_boxes[[open_order[j]]][2])
+        }      
+      }
+    }
+  }
+
+  if (strategy == 3) {
+    for (a in 1:nreps) {
+      #Card Box Allocation
+      box_numbers <- c(1:(2*n))
+      card_numbers <- c(sample(1:(2*n)))
+      cards_in_boxes <- list()      
+      open_order <- c(sample(1:(2*n), n))
+      for (i in 1:(2*n)) {
+        cards_in_boxes <- append(cards_in_boxes, list(c(box_numbers[i], card_numbers[i])))
+      }
+      for (j in 1:n) {
+        if (cards_in_boxes[[open_order[j]]][2] == k) {
+          counter[a] <- 1
+          break}
+      }
+    }
+  }
+  print(sum(counter)/nreps)
+}
+
+Pone(50,3,1,10000)
+Pone(50,3,2,10000)
+Pone(50,3,3,10000)
 
 ############################################################################################################################
 
@@ -173,18 +243,25 @@ Pall <- function(n, strategy, nreps) {
     
   }
   print(sum(all_counter)/nreps)
-  print(tail(all_counter, 20))
+  #print(tail(all_counter, 20))
   #print(cards_in_boxes)
-  print(counter)
+  #print(counter)
   
 }
 
-Pall(5,1,10000)
-Pall(5,2,10000)
-Pall(5,3,10000)
-Pall(50,1,10000)
-Pall(50,2,10000)
-Pall(50,3,10000)
+#Q3 
+
+for( i in c(5, 50)) {
+  for (j in (1:3)) {
+    Pall(i, j, 10000)
+  }
+}
+#Pall(5,1,10000)
+#Pall(5,2,10000)
+#Pall(5,3,10000)
+#Pall(50,1,10000)
+#Pall(50,2,10000)
+#Pall(50,3,10000)
 
 dloop <- function(n, nreps) { 
   
